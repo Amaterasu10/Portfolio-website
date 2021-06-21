32b95c26b047b770e4e24529b43049ff8827cdf6
John hinrich <johnhinrichggalindo@gmail.com>
2021-06-21 21:53:29 +0800

    UI and UX enhancement #1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("hello")
+const show = () => {
+    const projectContainer = document.querySelector('.project-container')
+
+    projectContainer.classList.toggle('container-opened')
+
+}
