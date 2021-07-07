a6fed96c07cd6ecfada5af376b23ec3f2ba84744
John hinrich <johnhinrichggalindo@gmail.com>
2021-07-07 20:05:46 +0800

    added more details to project and optimized transistion when clicking view project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 console.log("hello")
 const show = () => {
     const projectContainer = document.querySelector('.project-container')
+    const project = document.querySelector('iframe')
 
     projectContainer.classList.toggle('container-opened')
 
+    if(project.classList == ('project-opened')){
+        project.classList.toggle('project-opened')
+    }
+    else{
+        setTimeout(()=>{
+            project.classList.toggle('project-opened')        
+        }, 500)
+    }
+    
+    
 }
